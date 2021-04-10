41a41a2a06b41fb787ce8e34854db8f3f190a389
Effie Manyos <fmanyos@gmail.com>
2021-04-10 16:27:36 +0000

    saving

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -8,4 +8,24 @@ $(document).ready(function () {
         axis: "y",
         containment: "#projects"
     });
+    $("#btnAddProject").click(function () {
+        $("#project-dialog").dialog({
+            width: 400,
+            resizable: false,
+            modal: true,
+            buttons: {
+                "Add New Project": function () {
+                    var projectName = $("#new-project").val();
+                    $("<li><a href='#" + projectName + "'>" + projectName + "</a></li>").appendTo("#main");
+                    $("#projects").tabs("refresh");
+                    $("#new-project").val("");
+                    $(this).dialog("close");
+                },
+                "Cancel": function () {
+                    $("#new-project").val("");
+                    $(this).dialog("close");
+                }
+            }
+        });
+    });
 });
