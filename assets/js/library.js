457711d035e59680f664ea3f2d223e8098d2d764
Effie Manyos <fmanyos@gmail.com>
2021-04-20 20:05:09 +0000

    adding

--- assets/js/library.js
+++ assets/js/library.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     var item, title, author, publisher, bookLink, bookImg;
     var outputList = document.getElementById("list-output");
-    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q="
-    var placeHlder = ""
+    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
+    var placeHldr = ""
     var searchData;
 
     // Event listener for search button
@@ -28,7 +28,7 @@ $(document).ready(function () {
                         $("#book-title").animate({
                             'margin-top': '5px'
                         }, 1000); // Search box animation
-                        $(".book-list").css('visibility', 'visible');
+                        $(".book-list").css("visibility", "visible");
                         displayResults(response);
                     }
                 },
@@ -44,10 +44,64 @@ $(document).ready(function () {
     Function to display results in index.html
     #param res
     */
-    function displayResults(res) {
-        for (var i = 0; i < res.items.length; i += 2) {
-            item = res.items[i];
-            book - title
+    function displayResults(response) {
+        for (var i = 0; i < response.items.length; i += 2) {
+            item = response.items[i];
+            title1 = item.volumeInfo.title;
+            author1 = item.volumeInfo.authors;
+            publisher1 = item.volumeInfo.publisher;
+            bookLink1 = item.volumeInfo.previewLink;
+            bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
+            bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr;
+
+            item2 = response.items[i + 1];
+            title2 = item2.volumeInfo.title;
+            author2 = item2.volumeInfo.authors;
+            bookLink2 = item2.volumeInfo.previewLink;
+            bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
+            bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr;
+
+            // In production code, item.text should have the HTML entities escaped
+            outputList.innerHTML += '<div class="row mt-4">' +
+                formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
+                formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+                '</div>';
+
+            console.log(outputList);
         }
     }
-})
+
+    /*
+     * card element formatter using es6 backticks and templates (indivial card)
+     * @param bookImg title author publisher bookLink
+     * @return htmlCard
+     */
+    function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+        // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
+        var viewUrl = 'book.html?isbn=' + bookIsbn; // Constructing link for bookviewer
+        var htmlCard = `<div class="col-lg-6">
+       <div class="card" style="">
+         <div class="row no-gutters">
+           <div class="col-md-4">
+             <img src="${bookImg}" class="card-img" alt="...">
+           </div>
+           <div class="col-md-8">
+             <div class="card-body">
+               <h5 class="card-title">${title}</h5>
+               <p class="card-text">Author: ${author}</p>
+               <p class="card-text">Publisher: ${publisher}</p>
+               <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>`
+        return htmlCard;
+    }
+
+    // Handeling error for empty search box
+    function displayError() {
+        alert("search term can not be empty!")
+    }
+
+});
